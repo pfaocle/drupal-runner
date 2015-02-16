96e00e8995cb1cdab6a55f6c11faca28714b2e05
Paul Byrne <code@pfaocle.co.uk>
2015-02-16 00:51:50 +0000

    Better comment YamlBuildLoader class.

--- src/Drupal/Config/YamlBuildLoader.php
+++ src/Drupal/Config/YamlBuildLoader.php
@@ -6,15 +6,20 @@ use Symfony\Component\Config\Loader\FileLoader;
 use Symfony\Component\Yaml\Yaml;
 
 /**
- * @todo
+ * Loads and parses a configuration file as YAML.
  */
 class YamlBuildLoader extends FileLoader
 {
     /**
+     * Loads, parses and returns the given resource.
+     *
      * @param mixed $resource
+     *   The resource to load.
      * @param null $type
+     *   The resource type or null if unknown.
      *
      * @return array
+     *   The configuration file, parsed as YAML.
      */
     public function load($resource, $type = null)
     {
@@ -23,9 +28,12 @@ class YamlBuildLoader extends FileLoader
 
     /**
      * @param mixed $resource
+     *   The resource to load.
      * @param null $type
+     *   The resource type or null if unknown.
      *
      * @return bool
+     *   TRUE if the resource is supported.
      */
     public function supports($resource, $type = null)
     {
